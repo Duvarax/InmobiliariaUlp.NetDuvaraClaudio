f188734c638779134ae8dd7a1efc0dafe992f8fb
Duvara Claudio <duvaraclaudiojavier@gmail.com>
2023-03-28 17:14:48 -0300

    .

--- Models/Inmueble.cs
+++ Models/Inmueble.cs
@@ -1,22 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 namespace PracticaMVC.Models;
 
-public class Inquilino
+public class Inmueble
 {   
-    [Display(Name="Nro")]
     public int Id { get; set; }
     public string? Nombre { get; set; }
     public string? Apellido { get; set; }
     public string? Telefono { get; set; }
     public string? Dni { get; set; }
     public string? Email { get; set; }
+    public string? Clave { get; set; }
 
-    public Inquilino()
+    public Inmueble()
     {
        
     }
