74d194be8aa7f81974e57dc18db305ff75ff2e13
Duvara Claudio <duvaraclaudiojavier@gmail.com>
2023-03-30 15:20:44 -0300

    .

--- Models/Inmueble.cs
+++ Models/Inmueble.cs
@@ -3,12 +3,13 @@ namespace PracticaMVC.Models;
 public class Inmueble
 {   
     public int Id { get; set; }
-    public string? Nombre { get; set; }
-    public string? Apellido { get; set; }
-    public string? Telefono { get; set; }
-    public string? Dni { get; set; }
-    public string? Email { get; set; }
-    public string? Clave { get; set; }
+    public string? Direccion { get; set; }
+    public int? Ambientes { get; set; }
+    public int? Superficie { get; set; }
+    public decimal? Latitud { get; set; }
+    public decimal? Longitud { get; set; }
+    public int? PropietarioId { get; set; }
+    public Propietario Duenio {get; set;}
 
     public Inmueble()
     {
@@ -17,6 +18,6 @@ public class Inmueble
 
     public override string ToString()
     {
-        return Nombre + " " + Apellido;
+        return Direccion + " " + Duenio.ToString();
     }
 }
