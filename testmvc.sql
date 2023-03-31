d76a6493fb49af5937802634ac6d0d9a1467d7ed
Duvara Claudio <91039016+Duvarax@users.noreply.github.com>
2023-03-31 20:12:20 -0300

    update bd

--- testmvc.sql
+++ testmvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-03-2023 a las 21:06:06
+-- Tiempo de generación: 01-04-2023 a las 01:11:49
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.0.25
 
@@ -20,6 +20,58 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `testmvc`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contratos`
+--
+
+CREATE TABLE `contratos` (
+  `Id` int(11) NOT NULL,
+  `InquilinoId` int(11) NOT NULL,
+  `InmuebleId` int(11) NOT NULL,
+  `fechaInicio` datetime NOT NULL,
+  `fechaFinalizacion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `contratos`
+--
+
+INSERT INTO `contratos` (`Id`, `InquilinoId`, `InmuebleId`, `fechaInicio`, `fechaFinalizacion`) VALUES
+(15, 1, 3, '2023-03-31 19:32:00', '2023-03-25 19:32:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inmuebles`
+--
+
+CREATE TABLE `inmuebles` (
+  `Id` int(11) NOT NULL,
+  `Direccion` varchar(50) NOT NULL,
+  `Ambientes` int(11) NOT NULL,
+  `Superficie` int(11) NOT NULL,
+  `Latitud` decimal(10,0) NOT NULL,
+  `Longitud` decimal(10,0) NOT NULL,
+  `PropietarioId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inmuebles`
+--
+
+INSERT INTO `inmuebles` (`Id`, `Direccion`, `Ambientes`, `Superficie`, `Latitud`, `Longitud`, `PropietarioId`) VALUES
+(1, 'San Luisz', 2, 10, '5', '5', 1),
+(2, 'Cordoba', 2, 2, '4', '2', 2),
+(3, 'Cordoba', 2, 4, '1', '2', 1),
+(4, 'Buenos Aires', 5, 10, '52', '4', 1),
+(6, 'Mendoza', 455, 421, '13', '23', 1),
+(7, 'San Luisx', 2, 3, '4', '5', 1),
+(8, 'Buenos Aires', 2, 2, '3', '4', 2),
+(9, 'Cordoba', 2, 3, '5', '5', 1),
+(10, 'Narnia', 10, 10, '10', '10', 2);
 
 -- --------------------------------------------------------
 
@@ -41,7 +93,8 @@ CREATE TABLE `inquilinos` (
 --
 
 INSERT INTO `inquilinos` (`Id`, `Nombre`, `Apellido`, `Telefono`, `Dni`, `Email`) VALUES
-(1, 'Claudio', 'Duvara', '2665024712', '44358778', 'duvaraclaudiojavier@gmail.com');
+(1, 'Claudio', 'Duvara', '2665024712', '44358778', 'duvaraclaudiojavier@gmail.com'),
+(2, 'Javier', 'Duvara', '2665024712', '44358778', 'duvaraclaudiojavier@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -64,11 +117,27 @@ CREATE TABLE `propietarios` (
 --
 
 INSERT INTO `propietarios` (`Id`, `Nombre`, `Apellido`, `Telefono`, `Dni`, `Email`, `Clave`) VALUES
-(1, 'Claudio', 'Duvara', '2665024712', '44358778', 'duvaraclaudiojavier@gmail.com', 'asado');
+(1, 'Claudio', 'Duvara', '2665024712', '44358778', 'duvaraclaudiojavier@gmail.com', 'asado'),
+(2, 'Javier', 'Duvara', '231313131', '4123412412', 'duvaraclaudiojavier1@gmail.com', 'sadasd');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `contratos`
+--
+ALTER TABLE `contratos`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `InquilinoId` (`InquilinoId`),
+  ADD KEY `InmuebleId` (`InmuebleId`);
+
+--
+-- Indices de la tabla `inmuebles`
+--
+ALTER TABLE `inmuebles`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `PropietarioId` (`PropietarioId`);
 
 --
 -- Indices de la tabla `inquilinos`
@@ -87,16 +156,38 @@ ALTER TABLE `propietarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `contratos`
+--
+ALTER TABLE `contratos`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `inmuebles`
+--
+ALTER TABLE `inmuebles`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT de la tabla `inquilinos`
 --
 ALTER TABLE `inquilinos`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `propietarios`
 --
 ALTER TABLE `propietarios`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `inmuebles`
+--
+ALTER TABLE `inmuebles`
+  ADD CONSTRAINT `inmuebles_ibfk_1` FOREIGN KEY (`PropietarioId`) REFERENCES `propietarios` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
